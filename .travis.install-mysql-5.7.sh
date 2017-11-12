a49f38f83a60fe81cf5cf127510d4b7eebf925a9
zhennann <zhen.nann@icloud.com>
2017-11-12 12:08:56 +0800

    travis

--- .travis.install-mysql-5.7.sh
+++ .travis.install-mysql-5.7.sh
@@ -1,8 +1,8 @@
-echo mysql-apt-config mysql-apt-config/enable-repo select mysql-5.7-dmr | sudo debconf-set-selections
-wget http://dev.mysql.com/get/mysql-apt-config_0.2.1-1ubuntu12.04_all.deb
-sudo dpkg --install mysql-apt-config_0.2.1-1ubuntu12.04_all.deb
-sudo apt-get update -q
-sudo apt-get install -q -y -o Dpkg::Options::=--force-confnew mysql-server
+export MYSQL_APT_CONFIG_FILE=mysql-apt-config_0.7.2-1_all.deb
+wget http://dev.mysql.com/get/$MYSQL_APT_CONFIG_FILE
+sudo dpkg -i $MYSQL_APT_CONFIG_FILE
+sudo apt-get -y update
+sudo apt-get -y install mysql-server-5.7 mysql-client-core-5.7 mysql-client-5.7
 sudo mysql_upgrade -u root
 sudo service mysql restart
 mysql -u root -e "CREATE DATABASE `watch-articles` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci"
