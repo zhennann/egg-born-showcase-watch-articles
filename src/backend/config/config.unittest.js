5a69f24879b6ac9261c9e58d41727307c092c494
zhennann <zhen.nann@icloud.com>
2017-11-28 11:40:55 +0800

    update

--- src/backend/config/config.unittest.js
+++ src/backend/config/config.unittest.js
@@ -12,7 +12,7 @@ module.exports = appInfo => {
         host: '127.0.0.1',
         port: '3306',
         user: 'root',
-        password: '761030',
+        password: '',
         database: 'sys',
       },
     },
