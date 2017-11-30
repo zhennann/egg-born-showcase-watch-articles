eafde611e1b5b5fcc645d6918b686178ef93a049
zhennann <zhen.nann@icloud.com>
2017-11-30 11:23:12 +0800

    refactor: test middleware

--- src/module/aa-wa/backend/src/routes.js
+++ src/module/aa-wa/backend/src/routes.js
@@ -2,9 +2,16 @@ const version = require('./controller/version.js');
 const watch = require('./controller/watch.js');
 const article = require('./controller/article.js');
 
+const safeAccess = async function(ctx, next) {
+  if (ctx.ip !== '127.0.0.1') ctx.throw(403);
+  // next
+  await next();
+};
+
 module.exports = [
   { method: 'post', path: 'version/update', controller: version, middlewares: 'safeAccess' },
-  { method: 'post', path: 'watch/run', controller: watch, middlewares: 'safeAccess' },
+  // { method: 'post', path: 'watch/run', controller: watch, middlewares: 'safeAccess' },
+  { method: 'post', path: 'watch/run', controller: watch, middlewares: [ safeAccess ] },
   { method: 'post', path: 'watch/fetch', controller: watch },
   { method: 'post', path: 'article/list', controller: article },
   { method: 'post', path: 'article/item', controller: article },
