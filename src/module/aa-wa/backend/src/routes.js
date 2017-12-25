c5197625c0682b8a599f091a21c9c5fddc519817
zhennann <zhen.nann@icloud.com>
2017-12-25 18:42:29 +0800

    refactor: middleware:auth

--- src/module/aa-wa/backend/src/routes.js
+++ src/module/aa-wa/backend/src/routes.js
@@ -2,8 +2,8 @@ const version = require('./controller/version.js');
 const watch = require('./controller/watch.js');
 const article = require('./controller/article.js');
 
-const safeAccess = async function(ctx, next) {
-  if (ctx.ip !== '127.0.0.1') ctx.throw(403);
+const auth = async function(ctx, next) {
+  if (!ctx.isAuthenticated()) ctx.throw(401);
   // next
   await next();
 };
@@ -11,11 +11,10 @@ const safeAccess = async function(ctx, next) {
 module.exports = [
   { method: 'post', path: 'version/update', controller: version, middlewares: 'safeAccess' },
   { method: 'post', path: 'version/test', controller: version, middlewares: 'safeAccess' },
-  // { method: 'post', path: 'watch/run', controller: watch, middlewares: 'safeAccess' },
-  { method: 'post', path: 'watch/run', controller: watch, middlewares: [ safeAccess ] },
+  { method: 'post', path: 'watch/run', controller: watch, middlewares: 'safeAccess' },
   { method: 'post', path: 'watch/fetch', controller: watch },
   { method: 'post', path: 'article/list', controller: article },
   { method: 'post', path: 'article/item', controller: article },
-  { method: 'post', path: 'article/add', controller: article, middlewares: 'auth' },
-  { method: 'post', path: 'article/delete', controller: article, middlewares: 'auth' },
+  { method: 'post', path: 'article/add', controller: article, middlewares: [ auth ] },
+  { method: 'post', path: 'article/delete', controller: article, middlewares: [ auth ] },
 ];
